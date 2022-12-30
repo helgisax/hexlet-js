681728df4fc4b68a065ed9c706225c8e57d88eaa
helgisax <helgiingvarsson77@gmail.com>
2022-12-30 05:48:14 +0300

    fix function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,8 @@
 const capitalize = (text) => {
+  if (text.length === 0) {
+    return '';
+  }
+
   const firstChar = text[0].toUpperCase();
   const restSubstring = text.slice(1);
   return `${firstChar}${restSubstring}`;
